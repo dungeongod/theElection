a8fc79beba5967aea8388f24e9e9ff763a5607b2
nikhil722sharma <nikhil722sharma@gmail.com>
2019-06-12 13:54:54 +0530

    initial

---
+++ contracts/Election.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.5.8;
+
+contract Election {
+    // Read/write candidate
+    string public candidate;
+
+    // Constructor
+
+        constructor () public {
+            candidate = "Candidate 1";
+        }
+        
+    
+}
